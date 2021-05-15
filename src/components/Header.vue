13f6f14a52a8d4ecef0ee542ff024dc695064da0
heitordeve <contato@mahalilah.com.br>
2021-05-15 11:23:16 -0300

    Development - Creating Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-toolbar app>
+		<v-toolbar-title class="headline text-uppercase mr-4">
+			<span>Stock</span>
+			<span class="font-weight-light">Trader</span>
+		</v-toolbar-title>
+	</v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

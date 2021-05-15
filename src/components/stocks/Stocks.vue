ebca41d879cc22154b1455935c8de11fd5933903
heitordeve <contato@mahalilah.com.br>
2021-05-15 12:31:58 -0300

    Development - Creating shares component (Stocks)

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,24 @@
 <template>
-	<h1>Ações</h1>
+	<v-layout row wrap>
+		<Stock v-for="stock in stocks" :key="stock.id" />
+	</v-layout>
 </template>
 
 <script>
-export default {
+import Stock from './Stock'
 
+export default {
+	components: { Stock },
+	data() {
+		return {
+			stocks: [
+				{ id: 1, name: 'BMW', proce: 110 },
+				{ id: 2, name: 'GOOGLE', proce: 200 },
+				{ id: 3, name: 'APPLE', proce: 280 },
+				{ id: 4, name: 'TWITTER', proce: 180 },
+			]
+		}
+	}
 }
 </script>
 
